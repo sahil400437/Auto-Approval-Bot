53bfa4ba28c9838e380ffafab500d52ac91ba83a
Sahil kumar <133353982+sahil400437@users.noreply.github.com>
2025-01-22 20:33:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "6cc5c27041a110b47b3e804225e8ab33")
     BOT_TOKEN = getenv("BOT_TOKEN", "7618429387:AAEo7pd4lrxCXno2qpmDlkh9erM4sgCMikI")
     # Your Force Subscribe Channel Id Below 
-    CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
+    CHID = int(getenv("CHID", "-1001805846204")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
     SUDO = list(map(int, getenv("SUDO", "").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Sahil:<db_password>@cluster0.njg1t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
